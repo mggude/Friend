6946ce08b74c6cdf96ec7275d680044c14bd206d
Grace Gude <grace@Graces-MBP.localdomain>
2019-01-24 23:55:39 -0700

    Heroku is evil

---
+++ app/data/friends.js
@@ -0,0 +1,97 @@
+var friendsArray = [
+    {
+        "name":"Harry Potter",
+        "photo":"https://media2.giphy.com/media/o5HKScC1PflLO/giphy.gif?cid=3640f6095c4a9dca49524a7a4d9e165e",
+        "scores":[
+            5,
+            1,
+            2,
+            4,
+            5,
+            2,
+            3,
+            1,
+            4,
+            4
+          ]
+      },{
+          "name": "Hermione Granger",
+          "photo": "https://media1.giphy.com/media/l2JhL6uaJY7WzPbMs/giphy.gif?cid=3640f6095c4a9da7667072444dd45eab",
+          "scores": [
+            1,
+            5,
+            2,
+            1,
+            5,
+            5,
+            1,
+            1,
+            1,
+            5
+          ]
+      },{
+        "name": "Ron Weasley",
+        "photo": "https://media1.giphy.com/media/VwUquCGtIatGg/giphy.gif?cid=3640f6095c4a9d3f4b4f677667478a23",
+        "scores": [
+          3,
+          4,
+          5,
+          3,
+          2,
+          1,
+          5,
+          3,
+          4,
+          2
+        ]
+    },{
+        "name": "Neville Longbottom",
+        "photo": "https://media3.giphy.com/media/MNp1iqO3BMB20/giphy.gif?cid=3640f6095c4a9cf3396e4d3263c9f229",
+        "scores": [
+          1,
+          3,
+          5,
+          5,
+          2,
+          5,
+          1,
+          5,
+          5,
+          1
+        ]
+    },{
+        "name": "Luna Lovegood",
+        "photo": "https://media1.giphy.com/media/ksAw0VOOthw64/giphy.gif?cid=3640f6095c4a9cb431437a344591d388",
+        "scores": [
+          5,
+          1,
+          4,
+          5,
+          3,
+          5,
+          1,
+          5,
+          5,
+          1
+        ]
+    },{
+        "name": "Albus Dumbledore",
+        "photo": "https://media0.giphy.com/media/OU1marLMNNtnO/200w.webp?cid=3640f6095c4a8fac306165676b447957",
+        "scores": [
+          5,
+          5,
+          1,
+          1,
+          2,
+          5,
+          1,
+          3,
+          5,
+          5
+        ]
+    }
+      
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
