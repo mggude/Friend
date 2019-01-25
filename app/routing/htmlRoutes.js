ff7fa254fb8a701c5c4782f545b0b0585d3cd8da
Grace Gude <grace@Graces-MBP.localdomain>
2019-01-24 23:59:35 -0700

    routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -20,16 +20,7 @@ module.exports = function(app) {
     res.json(path.join(__dirname, "public/home.html"));
   });  
 
-  app.get("/home", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
-
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
-  });
-
-  // If no matching route is found default to home
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
